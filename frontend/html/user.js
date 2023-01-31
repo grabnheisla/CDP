6a6a36e32c1464412834c3949baaff4cb2d09695
spiro <robert@spies.at>
2023-01-31 23:46:16 +0100

    💄 Added UI Features

--- frontend/html/user.js
+++ frontend/html/user.js
@@ -1,3 +1,5 @@
+const cdpServer = "https://" + window.location.hostname + "/api"
+
 
 function setCookie(cname, cvalue, exmin) {
     const d = new Date();
@@ -20,6 +22,10 @@ function getCookie(cname){
     }
     return "";
 }
+function formatPrice(price){
+  formatter = new Intl.NumberFormat('de-AT');
+  return formatter.format(price);
+}
 var cookie_token = getCookie('token');
 
 const parseJwt = (token) => {
@@ -33,3 +39,73 @@ const parseJwt = (token) => {
 let tokenContent = parseJwt(cookie_token);
 let greeting = document.getElementById('greeting');
 greeting.innerText = 'Hallo '+tokenContent.displayname
+
+
+function createProductCard(product) {
+  console.log(product);
+  let btnGroup = document.createElement('div');
+  btnGroup.className='btn-group';
+  btnGroup.role='group';
+  btnGroup.style='width:100%';
+
+  let btn1 = document.createElement('button');
+  btn1.type='button';
+  btn1.className='btn btn-lg btn-primary btn-block';
+  btn1.innerHTML='<img src="icons/basket.svg">';
+
+  let btn2 = document.createElement('button');
+  btn2.type='button';
+  btn2.className='btn btn-lg btn-primary btn-block';
+  btn2.innerHTML='<img src="icons/send.svg">';
+
+  btnGroup.appendChild(btn1);
+  btnGroup.appendChild(btn2);
+
+  
+  let cardSubTitle = document.createElement('h6');
+  cardSubTitle.className = 'card-subtitle mb-2 text-muted'
+  cardSubTitle.innerText = formatPrice(product.price/100) + " €";
+  let cardTitle = document.createElement('h5');
+  cardTitle.className = 'card-title';
+  cardTitle.innerText = product.productname;
+
+  let cardImage = document.createElement('img');
+  cardImage.className="card-img-top";
+  cardImage.style="width:50%";
+  cardImage.src= 'icons/cup-hot-fill.svg';
+  
+
+  let cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+  cardBody.appendChild(cardImage);
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(cardSubTitle);
+  cardBody.appendChild(btnGroup);
+
+  let card = document.createElement('div');
+  card.className = 'card shadow cursor-pointer';
+  card.appendChild(cardBody);
+
+
+  let prodcol = document.createElement('div');
+  prodcol.className = 'col-sm-2 my-1';
+  prodcol.appendChild(card);
+
+  let productList = document.getElementById('products');
+  productList.appendChild(prodcol);
+
+
+}
+
+fetch(cdpServer + '/products')
+    .then(response => {
+      return response.json();
+    })
+    .then(products => {
+      console.log(products);
+      products.forEach(product => {
+        if (product.active) {
+          createProductCard(product);
+        }
+      });
+    })
